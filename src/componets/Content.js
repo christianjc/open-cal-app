063bf6036ecd512bd789f24556fb06707f7b9873
Christian Castaneda <christianjc_09@yahoo.com>
2022-03-11 18:45:36 -0600

    updated app content

---
+++ src/componets/Content.js
@@ -0,0 +1,17 @@
+
+function Button() {
+    return (
+        <button>Button</button>
+    )
+}
+
+function Content() {
+    return (
+      <div className="App-content">
+        <Button/>
+        <Button/>
+      </div>
+    )
+  }
+
+  export default Content
---
+++ src/componets/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+      <footer className="App-footer">
+        <small>This is the footer</small>
+      </footer>
+    )
+}
+
+export default Footer
